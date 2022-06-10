187a1d213d5728d2b0f3be326ae9ab1a053e5354
RajendraMallina <rajendramallina@gmail.com>
2022-06-10 08:54:21 +0530

    project added

---
+++ src/main/java/coding/maxima/codingmaxima/CodingmaximaApplication.java
@@ -0,0 +1,13 @@
+package coding.maxima.codingmaxima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingmaximaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingmaximaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coding/maxima/codingmaxima/CodingmaximaApplicationTests.java
@@ -0,0 +1,13 @@
+package coding.maxima.codingmaxima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingmaximaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
