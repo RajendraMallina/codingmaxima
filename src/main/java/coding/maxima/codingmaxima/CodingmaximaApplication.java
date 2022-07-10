d6d94d64591406efcc5fd2adf8ca72c323e3d547
RajendraMallina <rajendramallina@gmail.com>
2022-07-10 15:37:58 +0530

    updated animated card and optimized the gifs and images

--- src/main/java/coding/maxima/codingmaxima/CodingmaximaApplication.java
+++ src/main/java/coding/maxima/codingmaxima/CodingmaximaApplication.java
@@ -1,13 +1,25 @@
 package coding.maxima.codingmaxima;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 public class CodingmaximaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodingmaximaApplication.class, args);
+	}
+
+	@RequestMapping(path="/postevent", method = RequestMethod.POST)
+	public String getEndpoint(@RequestBody String str){
+		return "end point triggered";
 	}
 
 }
